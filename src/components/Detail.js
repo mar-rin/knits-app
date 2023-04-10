e9ed9d0b3ecbbc6eb15a3d8f5a23e854feeba4ab
mar-rin <125647550+mar-rin@users.noreply.github.com>
2023-04-10 11:31:57 +0300

    Add files via upload

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,66 +1,78 @@
-import * as React from 'react';
+import React, {useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import { useNavigate } from "react-router-dom";
+import { fieldNames } from '../utils/fieldNames';
+import { useParams, useNavigate } from "react-router-dom";
 
 
-export default function Detail({ active }) {
-    
+export default function Detail({ data, handleChange, handleSave }) {
+
+    const disabledFields = [false, false, false, true, false];
     const navigate = useNavigate();
-    
-    function FormField({ placeholder, title }) {
-      return (
-        <Grid item xs={12} md={6}>
-            <h4>{title}</h4>
-            <Box
-                component="form"
-                sx={{'& > :not(style)': { m: 1, width: '100%' }}}
-                noValidate
-                autoComplete="off"
-                >
-                <TextField 
-                    id="filled-basic"
-                    placeholder={placeholder}
-                    variant="filled"
-                />
-            </Box>
-        </Grid>
-      );
+    let { shipment } = useParams();
+    const [activeData, setActiveData] = useState({});
+
+
+    async function getDataSlice(){
+      setActiveData(data.filter(item => item.trackingNo === shipment))
     }
 
+    useEffect(() => {getDataSlice()}, [])
+
+
     return (
-        <div className='wrapper'>
-            <div className="detail-head"></div>
-            <div className="white-box">
-                <h3>SHIPMENT DETAIL</h3>
-                <Box sx={{ flexGrow: 1 }}>
-                    <Grid container spacing={3}>
-                        <FormField placeholder={active[0].orderNo} title="orderNo" />
-                        <FormField placeholder={active[0].date} title="date"/>
-                        <FormField placeholder={active[0].customer} title="customer"/>
-                        <FormField placeholder={active[0].trackingNo} title="trackingNo"/>
-                        <FormField placeholder={active[0].consignee} title="consignee"/>
-                        <FormField placeholder={active[0].status} title="status"/>
-                    </Grid>
-                </Box>
-                <div className="button-block confirm">
-                    <Stack direction="row" spacing={2}>
-                        <Button variant="contained" style={{ backgroundColor: "#2196f3" }}>SAVE</Button>
-                    </Stack>
-                    <Stack direction="row" spacing={2}>
-                        <Button 
-                            variant="contained" 
-                            style={{ backgroundColor: "#ff3d00" }}
-                            onClick={()=>navigate("/")}
+        <div>
+            { (activeData.length > 0)
+            ? <div className='wrapper'>
+                <div className="detail-head"></div>
+                <div className="white-box">
+                    <h3>SHIPMENT DETAIL</h3>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Grid container spacing={3}>
+                            {fieldNames.map((item, index) => (
+                                <Grid item xs={12} md={6} key={index}>
+                                    <h4>{item}</h4>
+                                    <Box
+                                        component="form"
+                                        sx={{'& > :not(style)': { m: 1, width: '100%' }}}
+                                        noValidate
+                                        autoComplete="off"
+                                    >
+                                        <TextField
+                                            name={item}
+                                            id="filled-basic"
+                                            placeholder={activeData[0][item]}
+                                            variant="filled"
+                                            onChange={handleChange}
+                                            disabled={disabledFields[index]}
+                                        />
+                                    </Box>
+                                </Grid>))}
+                        </Grid>
+                    </Box>
+                    <div className="button-block confirm">
+                        <Stack direction="row" spacing={2}>
+                            <Button
+                                variant="contained"
+                                style={{ backgroundColor: "#2196f3" }}
+                                onClick={ handleSave }
+                            >SAVE</Button>
+                        </Stack>
+                        <Stack direction="row" spacing={2}>
+                            <Button
+                                variant="contained"
+                                style={{ backgroundColor: "#ff3d00" }}
+                                onClick={()=>navigate("/")}
                             >CANCEL</Button>
-                    </Stack>
+                        </Stack>
+                    </div>
+
                 </div>
-        
             </div>
+            : <h2>IS FETCHING</h2> }
         </div>
-        
     )
 }
--- src/components/GeneralTable.js
+++ src/components/GeneralTable.js
@@ -9,20 +9,32 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Stack from '@mui/material/Stack';
 import IconButton from '@mui/material/IconButton';
+import SortByAlphaIcon from '@mui/icons-material/SortByAlpha';
 
 
+export default function GeneralTable({ data, handleSort, handleSortDate, handleEdit, handleDelete }) {
 
-export default function GeneralTable({ data, handleSort, handleEdit, handleDelete }) {
-
- 
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} >
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow className='table-header'>
             <TableCell>ORDERNO</TableCell>
-            <TableCell align="left">DELIVERYDATE</TableCell>
-            <TableCell align="left">CUSTOMER <button onClick={handleSort}>Sort</button></TableCell>
+            <TableCell align="left">DELIVERYDATE
+              <IconButton
+                  aria-label="edit"
+                  id="sort-date"
+                  onClick={handleSortDate}>
+                <SortByAlphaIcon style={{ color: "#2196f3" }}  />
+              </IconButton>
+            </TableCell>
+            <TableCell align="left" id="customer"><div id="customer-label">CUSTOMER</div>
+              <IconButton
+                  aria-label="edit"
+                  id="sort-customer"
+                  onClick={handleSort}>
+                <SortByAlphaIcon style={{ color: "#2196f3" }}  />
+              </IconButton></TableCell>
             <TableCell align="left">TRACKINGNO</TableCell>
             <TableCell align="left">STATUS</TableCell>
             <TableCell align="left">CONSIGNEE</TableCell>
@@ -35,13 +47,13 @@ export default function GeneralTable({ data, handleSort, handleEdit, handleDelet
               key={id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">{item.orderNo}</TableCell>
-              <TableCell align="left">{item.date}</TableCell>
-              <TableCell align="left">{item.customer}</TableCell>
-              <TableCell align="left">{item.trackingNo}</TableCell>
-              <TableCell align="left">{item.status}</TableCell>
-              <TableCell align="left">{item.consignee}</TableCell>
-              <TableCell align="left">
+              <TableCell component="th" scope="row"  style={{ color: "rgb(104, 104, 104)"}}>{item.orderNo}</TableCell>
+              <TableCell align="left" style={{ color: "rgb(104, 104, 104)"}}>{item.date}</TableCell>
+              <TableCell align="left" style={{ color: "rgb(104, 104, 104)"}}>{item.customer}</TableCell>
+              <TableCell align="left" style={{ color: "rgb(104, 104, 104)"}}>{item.trackingNo}</TableCell>
+              <TableCell align="left" style={{ color: "rgb(104, 104, 104)"}}>{item.status}</TableCell>
+              <TableCell align="left" style={{ color: "rgb(104, 104, 104)"}}>{item.consignee}</TableCell>
+              <TableCell align="left" >
                 <div className="button-block"> 
                   <Stack spacing={1} direction="row" >
                     <IconButton 
